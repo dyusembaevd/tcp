9da05a4d5bc724b242270803d8ef9ba722d8aec0
mohito22 <kpyshitejib@gmail.com>
2021-01-22 07:42:41 -0500

    Tcp

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mohito22/tcp
+
+go 1.15
---
+++ tcp.go
@@ -0,0 +1,41 @@
+package tcp
+
+import (
+	"net"
+)
+
+// TCPConfig ..
+type TCPConfig struct {
+	hostname string
+	port     string
+	conn     net.Conn
+}
+
+func NewConfig() *TCPConfig {
+	return &TCPConfig{}
+}
+
+func (t *TCPConfig) Connect(hostname, port string) error {
+	t.hostname = hostname
+	t.port = port
+	err := t.connection()
+	return err
+}
+
+func (t *TCPConfig) connection() error {
+	conn, err := net.Dial("tcp", t.hostname+":"+t.port)
+	t.conn = conn
+	return err
+}
+
+func (t *TCPConfig) ReadTCPMessage() []byte {
+	conn := t.conn
+	buffer := make([]byte, 1024)
+	conn.Read(buffer)
+	return buffer
+}
+
+func (t *TCPConfig) WriteTCPMessage(message []byte) error {
+	_, err := t.conn.Write([]byte(message))
+	return err
+}
