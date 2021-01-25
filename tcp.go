99f7657a23e22dc11aaff517b6808a7206e7180e
mohito22 <kpyshitejib@gmail.com>
2021-01-25 06:55:20 -0500

    timeout and deadlien added

--- tcp.go
+++ tcp.go
@@ -26,6 +26,7 @@ func (t *TCPConfig) Connect(hostname, port string) error {
 func (t *TCPConfig) connection() error {
 	var d net.Dialer
 	d.Timeout = 1 * time.Minute
+	d.Deadline = <-time.After(time.Minute)
 	conn, err := d.Dial("tcp", t.hostname+":"+t.port)
 	// conn, err := net.DialTimeout("tcp", t.hostname+":"+t.port, 1*time.Minute)
 	t.conn = conn
